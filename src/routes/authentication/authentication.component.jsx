e05b31fc33145e91ae26723bfe1ec5150f8b5317
Vu Pham <vdpham326@gmail.com>
2022-04-07 22:03:28 -0700

    created authentication page component alongside sign in form

---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import "./authentication.styles.scss";
+
+const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Authentication;
